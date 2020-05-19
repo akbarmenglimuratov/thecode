a42340540074f0f9a27c2f6db1b8384b5f389b4b
Akbar <akbarmenglimuratov@gmail.com>
2020-05-19 09:01:35 +0300

    static

--- thecode/settings.py
+++ thecode/settings.py
@@ -203,7 +203,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'thecode/media')
